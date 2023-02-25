a864224b70eeaa043a7f6ee0883b9f50d05fdb41
Dedit Hery Suprastyo <arek.nggantenk@gmail.com>
2023-02-25 23:56:35 +0700

    create show all film category for homework task 2-3

--- dvdRental/router.js
+++ dvdRental/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express();
 const pool = require("../queries.js");
 
-router.get("/film", (req, res) => {
+router.get("/films", (req, res) => {
   pool.query("SELECT*FROM film LIMIT 10", (err, result) => {
     if (err) {
       console.log(err);
@@ -13,6 +13,15 @@ router.get("/film", (req, res) => {
 
 router.get("/film/:id", (req, res) => {
   pool.query(`SELECT*FROM film WHERE film_id=${req.params.id}`, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.status(200).json(result.rows);
+  });
+});
+
+router.get("/films/categories", (req, res) => {
+  pool.query("SELECT*FROM category", (err, result) => {
     if (err) {
       console.log(err);
     }
