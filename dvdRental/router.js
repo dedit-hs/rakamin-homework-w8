c7540fa3894f2d41c93145407b90cb01432fc2b2
Dedit Hery Suprastyo <arek.nggantenk@gmail.com>
2023-02-25 23:52:37 +0700

    create show film depend on id for homework task 2-2

--- dvdRental/router.js
+++ dvdRental/router.js
@@ -11,4 +11,13 @@ router.get("/film", (req, res) => {
   });
 });
 
+router.get("/film/:id", (req, res) => {
+  pool.query(`SELECT*FROM film WHERE film_id=${req.params.id}`, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    res.status(200).json(result.rows);
+  });
+});
+
 module.exports = router;
