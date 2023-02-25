384037a98fb9b4fd3cb88ffe85c79e7d95a25775
Dedit Hery Suprastyo <arek.nggantenk@gmail.com>
2023-02-25 23:24:13 +0700

    create seeding for homework task 1

---
+++ db/seeding.sql
@@ -0,0 +1,12 @@
+INSERT INTO actor (first_name, last_name)
+VALUES ('Abu', 'Bakar');
+INSERT INTO actor (first_name, last_name)
+VALUES ('Umar', 'Ibnu Al Khattab');
+
+INSERT INTO actor (first_name, last_name)
+VALUES ('Usman', 'Ibnu Affan');
+
+INSERT INTO actor (first_name, last_name)
+VALUES ('Abu', 'Ibnu Abi Thalib');
+INSERT INTO actor (first_name, last_name)
+VALUES ('Khalid', 'Ibnu Waalid');
---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3003;
+
+const pool = require("./queries.js");
+
+pool.connect((err, res) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("Database Connected.");
+});
+
+app.listen(port, () => {
+  console.log(`Application running on port ${port}`);
+});
